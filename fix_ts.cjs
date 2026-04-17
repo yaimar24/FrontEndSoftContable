8906db64b1ccf06847dcc3bac602ee8b4c745b77
yaimar <yaimar096@gmail.com>
2026-04-17 16:45:04 -0500

    feat: Remove 'CREDITO' payment option and related logic from CreateCompras component

--- fix_ts.cjs
+++
@@ -1,8 +0,0 @@
-﻿const fs = require('fs');
-let p = 'src/presentation/pages/dashboard/compras/CreateCompras/CreateCompras.tsx';
-let v = fs.readFileSync(p, 'utf8');
-
-// Replace the stray )} with nothing
-v = v.replace(/<\/div>\s*\)\}\s*<\/section>/, "</div>\n          </section>");
-
-fs.writeFileSync(p, v);
